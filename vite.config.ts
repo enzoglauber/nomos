1009b4d6c9d7edc27f4e5c0d4f5b2f27397687b8
Enzo Glauber <enzojob@gmail.com>
2023-03-18 18:56:06 -0300

    config vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  }
 })
