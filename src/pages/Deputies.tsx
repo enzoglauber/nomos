4720296d93da2a1c0055aadcefbcef735afa1828
Enzo Glauber <enzojob@gmail.com>
2023-03-26 11:22:50 -0300

    base Deputies spect

--- src/pages/Deputies.tsx
+++ src/pages/Deputies.tsx
@@ -10,25 +10,27 @@ import { IStore, StoreContext } from '../contexts/Store'
 import useDeputies from '../hooks/useDeputies'
 
 function Deputies() {
-  const { page, items, setPage, setItems } = useContextSelector(
+  const { page, items, filter, setPage, setItems } = useContextSelector(
     StoreContext,
     (store: IStore) => store
   )
-  const { status, data, error } = useDeputies()
 
+  const { status, data, error } = useDeputies(page, filter, items)
+  console.log(status, error)
   return (
     <>
-      <Container maxWidth="lg" sx={{ my: 6 }}>
+      <Container maxWidth="lg" sx={{ my: 6 }} data-testid="deputies">
+        {status}LEGALLLLLLLLLLLLLL
         <SubHeader title="Deputados" subtitle="Veja abaixo a lista de deputados" />
         <Box sx={{ display: 'flex', flexWrap: 'wrap' }} mb={7} mt={6}>
           <DeputiesFilter />
         </Box>
         {status === 'loading' ? (
-          <div>Loading...</div>
+          <div data-testid="deputies-loading">Loading...</div>
         ) : status === 'error' ? (
-          <div>Error: {error.message}</div>
+          <div data-testid="deputies-error">Error: {error.message}</div>
         ) : (
-          <DeputiesList data={data.dados} />
+          <DeputiesList data={data.dados} data-testid="deputies-list" />
         )}
         <Box sx={{ display: 'flex', justifyContent: 'flex-end' }} mt={5}>
           <Pagination
