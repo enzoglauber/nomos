d0e8af0723d568623163a2780d058bb82bfec96c
Enzo Glauber <enzojob@gmail.com>
2023-03-31 12:57:14 -0300

    Deputies cy

--- cypress/e2e/deputies.cy.ts
+++ cypress/e2e/deputies.cy.ts
@@ -2,7 +2,6 @@ describe('Page Deputies', () => {
   beforeEach(() => {
     cy.viewport(1280, 720)
     cy.visit('/deputados')
-    
   })
 
   it('should visit path deputies and load data', () => {
@@ -36,27 +35,9 @@ describe('Page Deputies', () => {
 
     cy.get(select).children('input[aria-hidden="true"]').should('have.value', '8');
     cy.get('[data-testid="deputy-list"]').children().its('length').should('be.equal', 8)
-  });
+  })
 
   it("should type into filters", () => {
-    // cy.intercept('GET', '**/api/v2/deputados', (req) => {
-    //   delete req.headers['if-modified-since']
-    //   delete req.headers['if-none-match']
-    //   delete req.headers['proxy-connection']
-
-    //   req.continue(res => {
-    //     cy.should('be.greaterThan', res.headers['x-total-count'])
-    //   })
-    // }).as('getSettings')
-    
-    // cy.wait('@getSettings')     
-
-    // cy.wait('@getDeputies')
-    //   .its('request.headers')
-    //   .should('have.property', 'x-total-count')
-    //   .then(res => {
-    //     cy.should('be.greaterThan', parseInt(res))
-    //   })
     cy.get('[data-testid="deputies-filter"]')
       .find('#uf')
       .type('RJ{enter}')
@@ -71,46 +52,29 @@ describe('Page Deputies', () => {
       .find('#deputy')
       .type('h{enter}')
     cy.get('[data-testid="deputy-list"]').children().its('length').should('be.equal', 2)
-  });
 
-
-  // it('should typing in text deputy field', () => {
-  // })
-  /**
-   * 
-   * 
-   *  expect(response).to.have.property('headers')
-   * 
-   * cy.visit('http://localhost:3000/admin')
-   * cy.url().should('match', /login/)
-   * 
-   * cy.get('button[aria-label="page 1"]')
-      // .invoke('attr', 'aria-current')
-      // .should('eq', 'true')
-    .should('have.attr', 'aria-current', 'true')
-
-   *  cy.get('#launch-list').its('length').should('be.gte', 1).and('be.lt', 20)
-   * 
-   * 
-   *  it('sets auth cookie when logging in via form submission', function () {
-    // destructuring assignment of the this.currentUser object
-    const { username, password } = this.currentUser
-
-    cy.visit('/login')
-
-    cy.get('input[name=username]').type(username)
-
-    // {enter} causes the form to submit
-    cy.get('input[name=password]').type('${password}{enter}')
-
-    // we should be redirected to /dashboard
-    cy.url().should('include', '/dashboard')
-
-    // our auth cookie should be present
-    cy.getCookie('your-session-cookie').should('exist')
-
-    // UI should reflect this user being logged in
-    cy.get('h1').should('contain', 'jane.lane')
+    cy.get('[data-testid="deputies-filter"]')
+      .find('#deputy')
+      .clear()
+      .type('helio{enter}')
+    cy.get('[data-testid="deputy-list"]').children().its('length').should('be.equal', 1)
   })
-   */
+
+  it("should click at button detail and redirec to detail page", () => {
+    cy.get('[data-testid="deputies-filter"]')
+      .find('#deputy')
+      .clear()
+      .type('helio lopes{enter}')
+    cy.get('[data-testid="deputy-list"]').children().its('length').should('be.equal', 1)
+
+    cy.get('[data-testid="deputy-list"]')
+      .children()
+      .first()
+      .find('[data-testid="deputy-list-item-link"]')
+      .click()
+    
+    cy.on("url:changed", (url) => {
+      expect(url).to.contain("/deputados/204444")
+    })
+  })
 })
