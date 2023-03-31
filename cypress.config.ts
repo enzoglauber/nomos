44995496e1e8fd66fc8fac8755c6db729f5e7829
Enzo Glauber <enzojob@gmail.com>
2023-03-30 21:04:28 -0300

    deputies spec

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     }
   },
   e2e: {
+    experimentalStudio: true,
     baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,20 +1,97 @@
 describe('Page Deputies', () => {
   beforeEach(() => {
     cy.viewport(1280, 720)
+    cy.visit('/deputados')
+    
   })
 
-  // it('displays full header', () => {
-  //   cy.get('nav .desktop-menu').should('be.visible')
-  //   cy.get('nav .mobile-menu').should('not.be.visible')
-  // })
-  it('should visit path deputies', () => {
-    cy.visit('/deputados')
-    cy.get('#deputy').type('joi{enter}')
+  it('should visit path deputies and load data', () => {
+    cy.get('[data-testid="deputy-list"]')
+      .children()
+      .its('length')
+      .should('be.gte', 1)
+      .and('be.lte', 10)
   })
+
+  it("should change current page and change it", () => {
+    const container = '[data-testid="pagination"]'
+    cy.get('button[aria-label="page 1"]')
+      .should('have.attr', 'aria-current', 'true')
+
+    cy.get(container)
+      .find('[aria-label="Go to page 2"]')
+      .click();
+
+    cy.get('button[aria-label="page 2"]')
+      .should('have.attr', 'aria-current', 'true')
+  });
+
+  it("should check items per page and change it", () => {
+    const select = '[data-testid="pagination-select"]'
+    cy.get(select).children('input[aria-hidden="true"]').should('have.value', '10');
+
+    
+    cy.get(select).click();
+    cy.get('ul[role="listbox"]').children('li[data-value="8"]').click()
+
+    cy.get(select).children('input[aria-hidden="true"]').should('have.value', '8');
+    cy.get('[data-testid="deputy-list"]').children().its('length').should('be.equal', 8)
+  });
+
+  it("should type into filters", () => {
+    // cy.intercept('GET', '**/api/v2/deputados', (req) => {
+    //   delete req.headers['if-modified-since']
+    //   delete req.headers['if-none-match']
+    //   delete req.headers['proxy-connection']
+
+    //   req.continue(res => {
+    //     cy.should('be.greaterThan', res.headers['x-total-count'])
+    //   })
+    // }).as('getSettings')
+    
+    // cy.wait('@getSettings')     
+
+    // cy.wait('@getDeputies')
+    //   .its('request.headers')
+    //   .should('have.property', 'x-total-count')
+    //   .then(res => {
+    //     cy.should('be.greaterThan', parseInt(res))
+    //   })
+    cy.get('[data-testid="deputies-filter"]')
+      .find('#uf')
+      .type('RJ{enter}')
+    cy.get('[data-testid="deputy-list"]').children().its('length').should('be.equal', 10)
+
+    cy.get('[data-testid="deputies-filter"]')
+      .find('#party')
+      .type('PL{enter}')
+    cy.get('[data-testid="deputy-list"]').children().its('length').should('be.equal', 10)
+    
+    cy.get('[data-testid="deputies-filter"]')
+      .find('#deputy')
+      .type('h{enter}')
+    cy.get('[data-testid="deputy-list"]').children().its('length').should('be.equal', 2)
+  });
+
 
   // it('should typing in text deputy field', () => {
   // })
   /**
+   * 
+   * 
+   *  expect(response).to.have.property('headers')
+   * 
+   * cy.visit('http://localhost:3000/admin')
+   * cy.url().should('match', /login/)
+   * 
+   * cy.get('button[aria-label="page 1"]')
+      // .invoke('attr', 'aria-current')
+      // .should('eq', 'true')
+    .should('have.attr', 'aria-current', 'true')
+
+   *  cy.get('#launch-list').its('length').should('be.gte', 1).and('be.lt', 20)
+   * 
+   * 
    *  it('sets auth cookie when logging in via form submission', function () {
     // destructuring assignment of the this.currentUser object
     const { username, password } = this.currentUser
@@ -24,7 +101,7 @@ describe('Page Deputies', () => {
     cy.get('input[name=username]').type(username)
 
     // {enter} causes the form to submit
-    cy.get('input[name=password]').type(`${password}{enter}`)
+    cy.get('input[name=password]').type('${password}{enter}')
 
     // we should be redirected to /dashboard
     cy.url().should('include', '/dashboard')
